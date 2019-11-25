0339b24dada0f147be557cef69caa43f176b1328
Gino Cabullo <gino.cabullo@obf.ateneo.edu>
2019-11-25 11:58:31 +0800

    Updates

---
+++ precache-manifest.f908e3c9f57852f46070ece53efd6bc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "744d1c9fe6b197cc897b96f50892a3c3",
+    "url": "/spotify-low-memory-player/index.html"
+  },
+  {
+    "revision": "de5013118f81c4a3b366",
+    "url": "/spotify-low-memory-player/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "8851aed1611b251831cb",
+    "url": "/spotify-low-memory-player/static/css/main.674c9e0d.chunk.css"
+  },
+  {
+    "revision": "de5013118f81c4a3b366",
+    "url": "/spotify-low-memory-player/static/js/2.2e69f48a.chunk.js"
+  },
+  {
+    "revision": "8851aed1611b251831cb",
+    "url": "/spotify-low-memory-player/static/js/main.abc23495.chunk.js"
+  },
+  {
+    "revision": "0ae97ac083da9edebe63",
+    "url": "/spotify-low-memory-player/static/js/runtime-main.07d7c66e.js"
+  }
+]);
